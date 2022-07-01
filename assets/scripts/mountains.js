645b32705fbd0a8cadc071247fd132c6f6d172ad
Rich Will <richard.will@libertymutual.com>
2022-07-01 04:17:44 -0700

    everything works, everything looks okay

--- assets/scripts/mountains.js
+++ assets/scripts/mountains.js
@@ -22,11 +22,14 @@ function generateMtnCard(mountain){
     //clear out the tables previous results
     mtnCard.innerHTML = ""
     let card = ""
-        card += `   <div class="card" style="width: 20rem;">`
+        card += `   <div class="card" style="width: 80rem;">`
         card += `   <div class="card-body">`
         card += `   <h5 class="card-title">${mountain.name}</h5>`
+        card += `   <img src="assets/images/mountains/${mountain.img}" class="card-img">`  
+        card += `   <p class="card-text"></p>` 
         card += `   <p class="card-text">${mountain.desc}</p>` 
-        card += `   <img src="assets/images/mountains/${mountain.img}">`       
+        card += `   <p class="card-text">Effort: ${mountain.effort}</p>` 
+        card += `   <p class="card-text">Coordinates: ${mountain.coords}</p>`      
         card += `   </div>`
         card += `   </div>`
         
